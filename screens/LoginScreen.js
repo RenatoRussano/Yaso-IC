210df9647b385c4fd299bac630768f20d999406b
freitaslivia <xliviafreitasx@gmail.com>
2024-08-20 19:10:32 -0300

    layout de login alterado

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { LinearGradient } from 'expo-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState('');
@@ -45,8 +47,7 @@ export default function LoginScreen({ navigation }) {
         await AsyncStorage.removeItem('remember');
       }
       
-      // Aqui você pode adicionar a lógica para fazer o login
-      if (username === 'Fernando' && password === 'CP02Mobile') {
+      if (username === '' && password === '') {
         navigation.navigate('Home');
       } else {
         setError('Usuário ou senha incorretos');
@@ -57,61 +58,122 @@ export default function LoginScreen({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
+   
+  <View style={styles.container}>
+    <LinearGradient
+      colors={['rgba(96,48,160,1)',  'rgba(255,175,255,1)']}
+      style={styles.background}
+      start={{ x: 0, y: 0 }} 
+      end={{ x: 1, y: 0 }}  
+    />
+
+    <View>
+
+      <Image source={require('../key-visual/yaso1.png')} style={styles.logo} />
+      {error ? <Text style={styles.error}>{error}</Text> : null}
+
+    </View>
+    <View style={styles.backgroundWithButtons}>
       <View style={styles.contentContainer}>
-        <Image source={require('../assets/LOGOMOBILE2.png')} style={styles.logo} />
-        <Text style={[styles.title, { color: '#808080' }]}>Login</Text> 
-        {error ? <Text style={styles.error}>{error}</Text> : null}
         <TextInput
-          style={[styles.input, { borderRadius: 50, marginTop: 50, fontStyle: 'italic' }]}
-          placeholder="   Nome de Usuário"
+          style={[styles.input, { marginTop: 50, fontStyle: 'italic', fontColor: '9A9A9A' }]}
+          placeholder="nome"
           value={username}
           onChangeText={setUsername}
         />
         <TextInput
-          style={[styles.input, { borderRadius: 50, fontStyle: 'italic' }]}
-          placeholder="   Senha"
+          style={[styles.input, { fontStyle: 'italic' }]}
+          placeholder="senha"
           secureTextEntry={true}
           value={password}
           onChangeText={setPassword}
         />
-        <View style={styles.rememberContainer}>
-          <TouchableOpacity onPress={() => setRemember(!remember)}>
-            <View style={[styles.checkbox, remember ? styles.checked : null, { marginLeft: 10 }]}/> 
+        <View style={styles.rememberAndForgotContainer}>
+          <View style={styles.rememberContainer}>
+            <TouchableOpacity onPress={() => setRemember(!remember)}>
+              <View style={[styles.checkbox, remember ? styles.checked : null]} />
+            </TouchableOpacity>
+            <Text style={styles.rememberText}>lembrar senha</Text>
+          </View>
+          <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
+            <Text style={[styles.forgotPasswordText, styles.link]}>esqueci minha senha</Text>
           </TouchableOpacity>
-          <Text style={[styles.rememberText, { color: '#808080', marginLeft: 5 }]}>Lembrar senha</Text> 
         </View>
-        <TouchableOpacity style={[styles.enterButton, { width: '100%', marginTop: 50 }]} onPress={handleLogin}> 
-          <Text style={styles.enterButtonText}>Entrar</Text>
+      </View>
+      <TouchableOpacity style={[styles.enterButton]} onPress={handleLogin}>
+        <Text style={styles.enterButtonText}>Entrar</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorContainer}>
+        <View style={styles.line} />
+        <Text style={styles.separatorText}>ou</Text>
+        <View style={styles.line} />
+      </View>
+      <View style={[styles.googleButtonAndApple]}>
+        <TouchableOpacity style={styles.button}>
+          <Icon name="google" size={20} />
+          <Text style={styles.googleAndAppleButtonText}>Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Icon name="apple" size={20} />
+          <Text style={styles.googleAndAppleButtonText}>Apple</Text>
         </TouchableOpacity>
       </View>
-        <View style={styles.forgotPasswordContainer}>
-          <Text style={[styles.forgotPasswordText, { color: '#808080' }]}>Esqueceu a senha? </Text>
-          <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
-            <Text style={[styles.forgotPasswordText, styles.link, { color: 'blue' }]}>Recuperar senha</Text>
-          </TouchableOpacity>
-        </View>
+      <View>
+        <TouchableOpacity style={styles.buttonEmergency}>
+          <Image  source={require('../key-visual/emergencia.png')} style={styles.emergencyIcon} />
+          <Text style={styles.textEmergency}>emergência</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+  </View>
+
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundWithButtons: {
+    //display: 'flex',
+    paddingTop: 0,
+    marginTop: 70,
+    backgroundColor: '#ffff',
+    borderRadius: 10,
+    width: 360,
+    height: 600,
+    alignItems: 'center',
+    padding: 0,
+
+    shadowOffset: { width: 5, height: 2 },
+    shadowColor: '#171717',
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+
+    elevation: 3,
+  },
+
+
+  background: {
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  height: '150%',
+  },
   container: {
-    flex: 1,
+    display: 'flex',
     alignItems: 'center',
     padding: 20,
-    backgroundColor: 'white'
   },
   contentContainer: {
     width: '80%',
     alignItems: 'center',
   },
   logo: {
-    marginTop: 80,
-    width: '100%',
-    height: undefined,
-    aspectRatio: 4,
-    marginBottom: 20,
+    marginTop: 40,
+    width: 100,
+    height: 155,
+    aspectRatio: 2,
+    marginBottom: 10,
   },
   title: {
     fontSize: 24,
@@ -120,55 +182,127 @@ const styles = StyleSheet.create({
   input: {
     width: '100%',
     height: 40,
-    borderColor: '#B2BEBF',
+    borderColor: '#9A9A9A',
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
+    placeholderTextColor: "#9A9A9A",
+    color: '#9A9A9A',
+    borderRadius: 10,
+    width: 320,
+    height: 50,
+    marginTop: 5,
+    
   },
   error: {
     color: 'red',
     marginBottom: 10,
   },
-  rememberContainer: {
+  rememberAndForgotContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 10,
+  },
+    rememberContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
-    alignSelf: 'flex-start',
   },
   checkbox: {
-    width: 20,
-    height: 20,
+    width: 15,
+    height: 15,
     borderRadius: 5,
     borderWidth: 1,
     borderColor: '#B2BEBF',
   },
   checked: {
-    backgroundColor: '#ff006c',
+    backgroundColor: '#6030A0',
   },
   rememberText: {
-    fontSize: 16,
+    fontSize: 12,
+    color: '#9A9A9A', 
+    marginLeft: 3
   },
   enterButton: {
-    backgroundColor: '#ff006c',
+    backgroundColor: '#6030A0',
     paddingVertical: 10,
     borderRadius: 50,
+    width: '80%', 
+    marginTop: 15,
+    width: 320,
+    height: 50,
+    //flex: 1
   },
   enterButtonText: {
     color: 'white',
     fontSize: 16,
     textAlign: 'center',
   },
-forgotPasswordContainer: {
-  flexDirection: 'row',
-  position: 'absolute',
-  bottom: 0,
-  alignSelf: 'center',
-  marginBottom: 20,
-},
+
+  button:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#9A9A9A',
+    paddingVertical: 15,
+    paddingHorizontal: 35,
+    borderRadius: 10,
+    marginRight: 10,
+    //flex: 1
+  },
+
+    googleAndAppleButtonText: {
+    color: '#fff',
+    marginLeft: 10, 
+  },
+
+
+  googleButtonAndApple:{
+    paddingTop: 20,
+    flexDirection: 'row',
+    //flex: 1
+  },
+
+  buttonEmergency:{
+    paddingTop: 60,
+    alignItems: 'center',
+    flex: 1
+  },
+
+  emergencyIcon:{
+    width: 30,
+    height: 30,
+  },
+
+  textEmergency:{
+    fontSize: 12,
+    color: '#6030A0'
+  },
+
   forgotPasswordText: {
-    fontSize: 16,
+    fontSize: 12,
+    textDecorationLine: 'underline'
   },
   link: {
-    color: 'blue',
+    color: '#9A9A9A',
+  },
+
+  
+  separatorContainer: {
+    paddingTop: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '80%', 
+    alignSelf: 'center',
+    marginVertical: 20,
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: '#9A9A9A', 
+  },
+  separatorText: {
+    marginHorizontal: 10, 
+    color: '#9A9A9A', 
   },
 });
